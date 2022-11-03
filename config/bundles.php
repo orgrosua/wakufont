f7e2190bdf27a279b2fdc96895e94ce3b06e90dc
Joshua Gugun Siagian <suabahasa@gmail.com>
2022-11-03 14:30:14 +0800

    Adds CORS headers support for ^/api path

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
