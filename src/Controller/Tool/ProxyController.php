b905511a422e59077dfd8b41370270868c99daad
Joshua Gugun Siagian <suabahasa@gmail.com>
2023-03-02 00:53:39 +0800

    fix code style

--- src/Controller/Tool/ProxyController.php
+++ src/Controller/Tool/ProxyController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Tool;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,15 +22,15 @@ class ProxyController extends AbstractController
         if (empty($url)) {
             return new JsonResponse([
                 'error' => 'url is empty',
-            ], \Symfony\Component\HttpFoundation\Response::HTTP_NOT_FOUND);
+            ], Response::HTTP_NOT_FOUND);
         } elseif (! filter_var($url, FILTER_VALIDATE_URL)) {
             return new JsonResponse([
                 'error' => 'url is not valid',
-            ], \Symfony\Component\HttpFoundation\Response::HTTP_BAD_REQUEST);
+            ], Response::HTTP_BAD_REQUEST);
         } elseif (! str_starts_with($url, 'https://fonts.gstatic.com')) {
             return new JsonResponse([
                 'error' => 'url is not from google fonts',
-            ], \Symfony\Component\HttpFoundation\Response::HTTP_FORBIDDEN);
+            ], Response::HTTP_FORBIDDEN);
         }
 
         $filename = basename($url);
